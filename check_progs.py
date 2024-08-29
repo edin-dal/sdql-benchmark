3c1322a20cc5a6e4cfd61834d5cedab411f61e0b
amascolo <12738821+amascolo@users.noreply.github.com>
2024-08-29 20:51:56 +0100

    Improvements to progs symlink check in Python

--- check_progs.py
+++ check_progs.py
@@ -1,15 +1,16 @@
 import os
 import sys
+from pathlib import Path
 from typing import Final
 
-PROGS_SYMLINK: Final[str] = os.path.join(
-    os.path.dirname(os.path.realpath(__file__)), "progs"
+PROGS_SYMLINK: Final[Path] = Path(
+    os.path.join(os.path.dirname(os.path.realpath(__file__)), "progs")
 )
 
 
 def check_progs():
     if not (
-        os.path.islink(PROGS_SYMLINK) and os.path.isdir(os.readlink(PROGS_SYMLINK))
+        PROGS_SYMLINK.is_symlink() and PROGS_SYMLINK.exists() and PROGS_SYMLINK.is_dir()
     ):
         sys.exit(
             "Follow instructions in README to create a symlink to the 'sdql/progs' directory"
